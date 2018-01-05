754a952ad255de49c2c951c1438e9479272ce0d2
Przemek Jurkiewicz <hankusekbb@gmail.com>
2018-01-05 11:44:25 +0100

    scripts.js update

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var button = document.getElementByClassName('button');
+var button = document.getElementsByClassName('button');
 for (var i = 0; i <= button.length; i ++) {
 	var buttonInnerText = button[i].innerText;
 	alert(buttonInnerText);
